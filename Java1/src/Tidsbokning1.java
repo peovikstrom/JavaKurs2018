4d5de7ba240b9619f60d636f138a407d2d2b2f42
elev <elev@172.20.0.170>
2018-04-30 15:32:28 +0200

    Tidsbokning uppgift 1 på väg att bli tidsbokning 2. Påbörjad.

--- Java1/src/Tidsbokning1.java
+++ Java1/src/Tidsbokning1.java
@@ -1,102 +1,111 @@
-import java.io.*;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
-
+import java.io.IOException;
 public class Tidsbokning1 {
 
 		public static void main(String[] args) throws IOException {
+			
 			List<LocalDateTime[]> boknDatumLista = new ArrayList<>();
 								
 			String input = "";																		//Skapa variabeln input av typen sträng som är tom så att while satsen skall fungera
 			
-			InputStreamReader inputStream = new InputStreamReader(System.in);						//Läs in valet du gjort
-			BufferedReader br = new BufferedReader(inputStream);
+			String boknDatum = "", boknStartTid = "", boknSlutTid = "";
+			
+			Scanner scanner = new Scanner(System.in);
 			
 			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-						
-			while (!input.equalsIgnoreCase("q")) {													//Kör while satsen så länge inte någon trycker på "q"											
 
-			System.out.println("Välj (b) för bokning, (c) för att kolla bokningsschema eller avsluta(q)");	//Skriv ut vilka val du kan göra
-				
-			input = br.readLine();																//Läs in valet från bufferedreadern till variabeln input
-				
-			if (input.equalsIgnoreCase("b")) {													
-									
-					String boknDatum, boknStartTid, boknSlutTid;								//Skapa temporära String variabler för datum, starttid och sluttid  
-												
-					System.out.println("Skriv in datum du vill boka(t.ex. 2011-01-01):");		//Läs in datum från bufferedreadern till variabeln boknDatum
-					boknDatum = br.readLine();
-					
-					System.out.println("Skriv in Starttid för bokningen(t.ex. 12:00):");		//Läs in starttid från bufferedreadern till variabeln boknStartTid
-					boknStartTid = br.readLine();
-					
-					System.out.println("Skriv in Sluttid för bokningen(t.ex. 13:00):");			//Läs in sluttid från bufferedreadern till variabeln boknSlutTid
-					boknSlutTid = br.readLine();
-					
-					LocalDateTime startTid, slutTid;													
-					startTid = LocalDateTime.parse(boknDatum + " " + boknStartTid, formatter);
-					slutTid = LocalDateTime.parse(boknDatum + " " + boknSlutTid, formatter);
-					LocalDateTime[] q = new LocalDateTime[2];
-					
-					q[0] = startTid;
-					q[1] = slutTid;
-					
-					boolean dubbelbokning = tidenRedanTagen(boknDatumLista, startTid, slutTid);
-					
-					if (dubbelbokning) { 
-										System.out.println("Den tiden är redan tagen! Välj ny tid");
-										
-					} else {										
-					
-					
-						// -1 = startid < sluttid, 1 tvärtom  
-						if (startTid.compareTo(slutTid) < 0) {
-								boknDatumLista.add(q);
-								System.out.println("Du har gjort en bokning: " + boknDatum	+  " mellan klockan " + boknStartTid + " och " + boknSlutTid);
-						} else 
-							System.out.println("Sluttiden får inte vara mindre än eller samma som starttiden.");
-						}
-										
-					
-							
-				} else 
-				if (input.equalsIgnoreCase("c")) {						
-					for (int i = 0; boknDatumLista.size() > i; i++) {
-						DateTimeFormatter formatterTime = DateTimeFormatter.ofPattern("HH:mm");
-						
-						String startTidString = boknDatumLista.get(i)[0].format(formatterTime);
-						String slutTidString = boknDatumLista.get(i)[1].format(formatterTime);
-						
-						DateTimeFormatter formatterDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-						
-						String bokningsDatumString = boknDatumLista.get(i)[0].format(formatterDate);
-						
-						System.out.println("Bokning Datum: " + bokningsDatumString + " Starttid: " + startTidString + " Sluttid: " + slutTidString);
-						
-					}
-				} else {
-					if (input.equalsIgnoreCase("q")) {
-						System.exit(0); 
-					} else
-						return;																//i så fall återvänd till början av if loopen
-						
-				}
+			LocalDateTime startTid = LocalDateTime.now();
+			LocalDateTime slutTid = LocalDateTime.now();
 			
-			}
+			LocalDateTime[] q = new LocalDateTime[2];
+			
+			while (!input.equalsIgnoreCase("q")) {													//Kör while satsen så länge inte någon trycker på "q"											
+					System.out.println("Choose (b) for booking, (c) to check allready made bookings or quit(q)");	//Skriv ut vilka val du kan göra
+					input = scanner.nextLine();															
+						
+					if (input.equalsIgnoreCase("b")) {													
+						try {
+							System.out.println("Enter the date you want to book. (ex. 2011-01-01):");		
+							boknDatum = scanner.nextLine();
+							
+							System.out.println("Enter the Starttime for your booking. (t.ex. 12:00):");		
+							boknStartTid = scanner.nextLine();
+				
+							System.out.println("Enter the Quittime for your booking. (t.ex. 13:00):");			
+							boknSlutTid = scanner.nextLine();
+								
+							startTid = LocalDateTime.parse(boknDatum + " " + boknStartTid, formatter);
+							
+							slutTid = LocalDateTime.parse(boknDatum + " " + boknSlutTid, formatter);
+							q[0] = startTid;
+							q[1] = slutTid;
+							} 
+								catch (DateTimeException e) {
+									System.out.println("Wrong date or time format. Date should be YYYY-MM-DD and Time HH:MM");
+								}
+					
+							boolean dubbelbokning = tidenRedanTagen(boknDatumLista, startTid, slutTid);
+							
+							if (dubbelbokning) { 
+												System.out.println("That time is already taken. Choose another time.");
+												
+							} else {										
+							
+							
+								// -1 = startid < sluttid, 1 startid > sluttid  
+								if (startTid.compareTo(slutTid) < 0) {
+										boknDatumLista.add(q);
+										System.out.println("You have made a booking: " + boknDatum	+  " between " + boknStartTid + " and " + boknSlutTid);
+								} else {
+									if (startTid.isEqual(slutTid) || startTid.isAfter(slutTid)) 
+									 System.out.println("Quittime cannot be greater or equal to Starttime. Choose another time.");
+								}
+							}					
+								
+						} else {
+						if (input.equalsIgnoreCase("c")) {						
+							if (boknDatumLista.size() > 0) {
+								for (int i = 0; boknDatumLista.size() > i; i++) {
+										DateTimeFormatter formatterTime = DateTimeFormatter.ofPattern("HH:mm");
+									
+										String startTidString = boknDatumLista.get(i)[0].format(formatterTime);
+										String slutTidString = boknDatumLista.get(i)[1].format(formatterTime);
+									
+										DateTimeFormatter formatterDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+									
+										String bokningsDatumString = boknDatumLista.get(i)[0].format(formatterDate);
+									
+										System.out.println("Booking Date: " + bokningsDatumString + " Starttime: " + startTidString + " Quittime: " + slutTidString);
+									
+								}
+							} else
+								System.out.println("No bookings found.");
+						} else {
+							if (input.equalsIgnoreCase("q")) {
+								System.exit(0); 
+							} else
+								return;																//i så fall återvänd till början av if loopen
+								
+						}
+					} 
+			} scanner.close();
+			
 		}
-
-		private static boolean tidenRedanTagen(List<LocalDateTime[]> boknDatumLista, LocalDateTime nyStartTid,
-				LocalDateTime nySlutTid) {
-			for (int i = 0; boknDatumLista.size() > i; i++) {
+		private static boolean tidenRedanTagen(List<LocalDateTime[]> boknDatumLista, LocalDateTime nyStartTid, LocalDateTime nySlutTid) {
+			int t = boknDatumLista.size();
+			for (int i = 0; t > i; i++) {
 					
 					LocalDateTime[] befintligBokning = boknDatumLista.get(i);
-					boolean case1 = befintligBokning[0].isBefore(nyStartTid) && befintligBokning[1].isAfter(nyStartTid) && befintligBokning[0].isBefore(nySlutTid) && befintligBokning[1].isAfter(nySlutTid);
-					boolean case2 = befintligBokning[0].isAfter(nyStartTid)  &&  befintligBokning[1].isAfter(nyStartTid) && befintligBokning[0].isBefore(nySlutTid) && befintligBokning[1].isAfter(nySlutTid);
-					boolean case3 = befintligBokning[0].isBefore(nyStartTid) && befintligBokning[1].isAfter(nyStartTid) && befintligBokning[0].isBefore(nySlutTid) && befintligBokning[1].isBefore(nySlutTid);
-					boolean case4 = befintligBokning[0].isAfter(nyStartTid) && befintligBokning[1].isAfter(nyStartTid) && befintligBokning[0].isBefore(nySlutTid) && befintligBokning[1].isBefore(nySlutTid);
+					boolean case1 = befintligBokning[0].isAfter(nyStartTid) && befintligBokning[1].isAfter(nySlutTid) /*&& befintligBokning[0].isBefore(nySlutTid) && befintligBokning[1].isAfter(nySlutTid)*/;
+					boolean case2 = befintligBokning[0].isBefore(nyStartTid)  &&  befintligBokning[1].isBefore(nySlutTid) /*&& befintligBokning[0].isBefore(nySlutTid) && befintligBokning[1].isAfter(nySlutTid)*/;
+					boolean case3 = befintligBokning[0].isAfter(nyStartTid) && befintligBokning[1].isEqual(nySlutTid) /*&& befintligBokning[0].isBefore(nySlutTid) && befintligBokning[1].isBefore(nySlutTid)*/;
+					boolean case4 = befintligBokning[0].isEqual(nyStartTid) && befintligBokning[1].isBefore(nySlutTid) /*&& befintligBokning[0].isBefore(nySlutTid) && befintligBokning[1].isBefore(nySlutTid)*/;
+					boolean case5 = befintligBokning[0].isBefore(nyStartTid) && befintligBokning[1].isAfter(nySlutTid) /*&& befintligBokning[0].isBefore(nySlutTid) && befintligBokning[1].isEqual(nySlutTid)*/;
+					boolean case6 = befintligBokning[0].isAfter(nyStartTid) && befintligBokning[1].isBefore(nySlutTid) /*&& befintligBokning[0].isAfter(nySlutTid) && befintligBokning[1].isEqual(nySlutTid)*/;
 					
-					if(case1 || case2 || case3 || case4) {
+					if(case1 || case2 || case3 || case4 || case5 || case6) {
 						return true;
 					}
 			}
