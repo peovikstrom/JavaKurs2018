2e78472841cc7e4f268b02fcd83cdc4c33413a1d
peovikstrom <38127071+peovikstrom@users.noreply.github.com>
2018-05-14 10:59:12 +0200

    Fixat Tidsbokning2. Kan någon kolla.

--- Java1/src/Tidsbokning2.java
+++ Java1/src/Tidsbokning2.java
@@ -1,6 +1,8 @@
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+
+//import gammalt.Bokning;
 public class Tidsbokning2 {
 
 		public static void main(String[] args) {
@@ -18,8 +20,6 @@ public class Tidsbokning2 {
 			LocalDateTime startTid = LocalDateTime.now();
 			LocalDateTime slutTid = LocalDateTime.now();
 			
-			
-			
 			while (!input.equalsIgnoreCase("q")) {													//Kör while satsen så länge inte någon trycker på "q"											
 					LocalDateTime[] q = new LocalDateTime[2];
 					System.out.println("Choose (b) for booking, (c) to check allready made bookings or quit(q)");	//Skriv ut vilka val du kan göra
@@ -27,56 +27,44 @@ public class Tidsbokning2 {
 					
 					if (input.equalsIgnoreCase("b")) {													
 						try {
-							System.out.println("Enter the date you want to book. (ex. 2011-01-01):");		
-							boknDatum = scanner.nextLine();
-							
-							System.out.println("Enter the Starttime for your booking. (ex. 12:00):");		
-							boknStartTid = scanner.nextLine();
+								System.out.println("Enter the date you want to book. (ex. 2011-01-01):");		
+								boknDatum = scanner.nextLine();
+								
+								System.out.println("Enter the Starttime for your booking. (ex. 12:00):");		
+								boknStartTid = scanner.nextLine();
+					
+								System.out.println("Enter the Quittime for your booking. (ex. 13:00):");			
+								boknSlutTid = scanner.nextLine();
+									
+								
+								
+								startTid = LocalDateTime.parse(boknDatum + " " + boknStartTid, formatter);
+								
+								slutTid = LocalDateTime.parse(boknDatum + " " + boknSlutTid, formatter);
+								
+								q[0] = startTid;
+								q[1] = slutTid;
+								
+								boolean dubbelbokning = tidenRedanTagen(boknDatumLista, startTid, slutTid);
+								
+								if (dubbelbokning) { 
+									int bStT = Integer.parseInt("" + startTid.getHour() + startTid.getMinute());
+									int bSlT = Integer.parseInt("" + slutTid.getHour() + slutTid.getMinute());
 				
-							System.out.println("Enter the Quittime for your booking. (ex. 13:00):");			
-							boknSlutTid = scanner.nextLine();
-								
-							
-							
-							startTid = LocalDateTime.parse(boknDatum + " " + boknStartTid, formatter);
-							
-							slutTid = LocalDateTime.parse(boknDatum + " " + boknSlutTid, formatter);
-							q[0] = startTid;
-							q[1] = slutTid;
-							
-							boolean dubbelbokning = tidenRedanTagen(boknDatumLista, startTid, slutTid);
-							
-							if (dubbelbokning) { 
-								boknDatumLista.add(q);
-								System.out.println("You have made a booking: " + boknDatum	+  " between " + boknStartTid + " and " + boknSlutTid);
-								/*int si = boknDatumLista.size(); 
-								if (si > 0) {
-									for (int i = 0; boknDatumLista.size() > i; i++) {
-											DateTimeFormatter formatterTime = DateTimeFormatter.ofPattern("HH:mm");
-										
-											String startTidString = boknDatumLista.get(i)[0].format(formatterTime);
-											String slutTidString = boknDatumLista.get(i)[1].format(formatterTime);
-										
-											DateTimeFormatter formatterDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-										
-											String bokningsDatumString = boknDatumLista.get(i)[0].format(formatterDate);
-										
-											System.out.println("Booking " + i + " Date: " + bokningsDatumString + " Starttime: " + startTidString + " Quittime: " + slutTidString);
-										
-									}
-								} else
-									System.out.println("No bookings found.");*/
-												
-							} else {
-									System.out.println("That time is already taken. Choose another time.");
-	
-									}		
+									if (bStT < bSlT) {
+										boknDatumLista.add(q);
+										System.out.println("You have made a booking: " + boknDatum	+  " between " + boknStartTid + " and " + boknSlutTid);
+										} else {
+											System.out.println("Quit Time can't be earlier then Start Time");
+											}
+								} else {
+										System.out.println("That time is already taken. Choose another time.");
+										}		
 						} 
-						catch (DateTimeException e) {
+							catch (DateTimeException e) {
 								System.out.println("Wrong date or time format. Date should be YYYY-MM-DD and Time HH:MM");
-						}					
-								
-						} else {
+							}					
+					} else {
 							if (input.equalsIgnoreCase("c")) {						
 								if (boknDatumLista.size() > 0) {
 									for (int i = 0; boknDatumLista.size() > i; i++) {
@@ -89,22 +77,17 @@ public class Tidsbokning2 {
 									
 										String bokningsDatumString = boknDatumLista.get(i)[0].format(formatterDate);
 									
-										System.out.println("Booking " + i + " Date: " + bokningsDatumString + " Starttime: " + startTidString + " Quittime: " + slutTidString);
-									
-								}
-							} else
-								System.out.println("No bookings found.");
-						} else {
-							if (input.equalsIgnoreCase("q")) {
-								System.exit(0); 
-							} else
-								return;																//i så fall återvänd till början av if loopen
-								
-						}
-					} 
+										System.out.println("Booking " + (i + 1) + " Date: " + bokningsDatumString + " Starttime: " + startTidString + " Quittime: " + slutTidString);
+									}
+								} else
+									System.out.println("No bookings found.");
+							} else {
+									if (input.equalsIgnoreCase("q")) {
+										System.exit(0); 
+									} 
+							}
+					}
 				} scanner.close();
-			
-			
 		} 
 		
 		private static boolean tidenRedanTagen(List<LocalDateTime[]> boknDatumLista, LocalDateTime nyStartTid, LocalDateTime nySlutTid) {
