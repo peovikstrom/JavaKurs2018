9876ef738d604eacb4e20d60e90a7d0853ed82a5
peovikstrom <38127071+peovikstrom@users.noreply.github.com>
2018-05-14 09:49:01 +0200

    Fixat de fel som fanns i Tidsbokning3.

---
+++ Java1/src/gammalt/Frisor.java
@@ -0,0 +1,46 @@
+package gammalt;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Frisor {
+	
+	List<Bokning> bokningar = new ArrayList<Bokning>();
+	String frisorName;
+	
+	public Frisor(String frisorName) {
+		this.frisorName = frisorName;
+	}
+	
+	public void printBokning() {
+		System.out.println(bokningar);
+	}
+	
+	public void addBokning(Bokning newBokning) {
+		bokningar.add(newBokning);
+	}
+	
+	public void getFrisorName () {
+		System.out.print(frisorName);
+	}
+	
+	public boolean isFrisorFree(Bokning newBokning) {
+		boolean check = true;
+		if (check) {
+			for (Bokning v : bokningar) {
+				
+				boolean case1 = (v.getStartTid().isAfter(newBokning.getStartTid()) && v.getStartTid().isAfter(newBokning.getSlutTid()));
+				boolean case2 = (v.getSlutTid().isBefore(newBokning.getStartTid()) && v.getSlutTid().isBefore(newBokning.getSlutTid()));
+								
+				if (case1 || case2) {
+					check = true;
+					break;
+				}	
+					else {
+							check = false;
+							break;
+						}			
+			}
+			return check;
+		} else return check;
+	}
+}
