089bd91f82926a910b5d691eaf32f74b40302531
peovikstrom <38127071+peovikstrom@users.noreply.github.com>
2018-05-17 13:22:07 +0200

    Sparar igen uppgift2.

--- Java1/src/Main.java
+++ Java1/src/Main.java
@@ -8,9 +8,27 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		
 		while (!input.equalsIgnoreCase("q")) {
-			
+			List<Integer> value = ArrayList<Integer>();
+			Integer value2 = 0;
+			Integer sum = 0;
+			System.out.println("choose (a) to add, choose (s) to subtract, choose (m) to multiply, choose (d) to divide or (q) to quit");
+			input = scanner.nextLine();
+			if (input.equalsIgnoreCase("a")) {
+				input = scanner.nextLine();
+				typeInValue(input);
+			}
+
 		}
-
 	}
-
+		
+	private static Integer typeInValue(String input) {
+		Integer value = 0;
+		try {
+			value = Integer.parseInt(input);
+		} catch (NumberFormatException e) {
+			System.out.println("You need to type in numbers!");
+		}
+	 return value;	
+	}
+	
 }
