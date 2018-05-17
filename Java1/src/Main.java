0d1994b0a7f7cfa323f195e65df7951f3d33b2bf
peovikstrom <38127071+peovikstrom@users.noreply.github.com>
2018-05-17 13:02:07 +0200

    Första koden inlagd i uppgift2.

--- Java1/src/Main.java
+++ Java1/src/Main.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		String input = "";
 		
+		Scanner scanner = new Scanner(System.in);
+		
+		while (!input.equalsIgnoreCase("q")) {
+			
+		}
 
 	}
 
