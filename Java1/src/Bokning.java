7e67ab65e37517a41ee649024bb0a5c5351c2f4d
peovikstrom <38127071+peovikstrom@users.noreply.github.com>
2018-05-24 10:51:42 +0200

    Pushar och commitar Tidsbokning3. Sparar, ser över den nu.

---
+++ Java1/src/Bokning.java
@@ -0,0 +1,56 @@
+import java.time.LocalDateTime;
+
+public class Bokning {
+	private LocalDateTime startTid;
+	private LocalDateTime slutTid;
+	private int uppskPris;
+	
+	public Bokning(LocalDateTime startTid, LocalDateTime slutTid, int uppskPris) {
+		this.startTid = startTid;
+		this.slutTid = slutTid;
+		this.uppskPris = uppskPris;
+	}
+	
+	public LocalDateTime getStartTid() {
+		return startTid;
+	}
+
+	public LocalDateTime getSlutTid() {
+		return slutTid;
+	}
+
+	public int getUppskPris() {
+		return uppskPris;
+	}
+
+	@Override
+	public String toString() {
+		int min;
+		if ((slutTid.getHour() - startTid.getHour()) == 0) {
+			min = ((slutTid.getMinute() - startTid.getMinute()) * 20); 
+			uppskPris = 50 + (min / 10);
+			} else
+				{ if (((slutTid.getHour() - startTid.getHour()) > 1) || ((slutTid.getMinute() - startTid.getMinute()) > 1)){
+					min = ((slutTid.getMinute() - startTid.getMinute()) * 20);
+					uppskPris = 150 + ((slutTid.getHour() - startTid.getHour()) * 50) + (min / 10);
+					} else { 
+						if ((slutTid.getHour() - startTid.getHour()) == 1) {
+							uppskPris = 200;
+						}
+					}
+				}
+
+		String strStart = startTid.getYear() + "-" + addZeroToInt(startTid.getMonthValue()) + "-" + addZeroToInt(startTid.getDayOfMonth()) + " " + addZeroToInt(startTid.getHour()) + ":" + addZeroToInt(startTid.getMinute());
+		strStart += " - " + addZeroToInt(slutTid.getHour()) + ":" + addZeroToInt(slutTid.getMinute()) + " Pris: " + uppskPris + "kr";
+		return strStart;	
+	}
+
+	private String addZeroToInt(int integer) {
+		if(integer < 10) {
+			return "0" + integer;
+		} else {
+			return integer + "";
+		}
+	}
+	
+}
---
+++ Java1/src/Tidsbokning3.java
@@ -0,0 +1,93 @@
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Tidsbokning3 {
+
+	public static void main(String[] args) {
+			
+		List<Frisor> frisor = new ArrayList<>();
+		
+		Frisor kalle = new Frisor("Kalle");
+		Frisor svenne = new Frisor("Svenne");
+		Frisor anna = new Frisor("Anna");
+		
+		frisor.add(kalle);
+		frisor.add(svenne);
+		frisor.add(anna);
+		
+		String input = "";																		//Skapa variabeln input av typen str�ng som �r tom s� att while satsen skall fungera
+		
+		String boknDatum = "", boknStartTid = "", boknSlutTid = "";
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		
+		LocalDateTime startTid = null;
+		LocalDateTime slutTid = null;
+		int uppskPris = 0;
+				
+		while (!input.equalsIgnoreCase("q")) {													//K�r while satsen s� l�nge inte n�gon trycker p� "q"											
+			System.out.println("Choose (b) for booking, (c) to check allready made bookings or quit(q)");	//Skriv ut vilka val du kan g�ra
+			input = scanner.nextLine();															
+			Bokning newBokning = null;
+			boolean checkIfWrongInput = true;
+			if (input.equalsIgnoreCase("b")) {
+				
+				try {
+						System.out.println("Enter the date you want to book. (ex. 2011-01-01):");		
+						boknDatum = scanner.nextLine();
+						
+						System.out.println("Enter the Starttime for your booking. (ex. 12:00):");		
+						boknStartTid = scanner.nextLine();
+	
+						System.out.println("Enter the Quittime for your booking. (ex. 13:00):");			
+						boknSlutTid = scanner.nextLine();
+	
+											
+						startTid = LocalDateTime.parse(boknDatum + " " + boknStartTid, formatter);
+						slutTid = LocalDateTime.parse(boknDatum + " " + boknSlutTid, formatter);
+						uppskPris = 0;
+						
+						int bStT = Integer.parseInt("" + startTid.getHour() + startTid.getMinute());
+						int bSlT = Integer.parseInt("" + slutTid.getHour() + slutTid.getMinute());
+	
+						if (bStT < bSlT) {
+							newBokning = new Bokning(startTid,slutTid,uppskPris);
+							} else {
+								System.out.println("Quit Time can't be earlier then Start Time");
+								checkIfWrongInput = false;
+								}
+					}
+						catch (DateTimeException e) {
+							System.out.println("Wrong date or time format. Date should be YYYY-MM-DD, Time HH:MM.");
+							checkIfWrongInput = false;
+						}
+					if (checkIfWrongInput) {
+					boolean ingenLedig = true;
+					for (Frisor f : frisor) {
+						if (f.isFrisorFree(newBokning)) {
+							f.addBokning(newBokning);
+							ingenLedig = false;
+							break;
+						}
+					} if (ingenLedig) {
+						System.out.println("No Hairdresser is free that time.");
+					} else {
+							System.out.println("Booked!");
+							
+						} 
+					}
+			} 
+			if (input.equalsIgnoreCase("c")) {
+				System.out.print("Hairdresser "); kalle.getFrisorName(); System.out.print(" "); kalle.printBokning();
+				System.out.print("Hairdresser "); svenne.getFrisorName(); System.out.print(" "); svenne.printBokning();
+				System.out.print("Hairdresser "); anna.getFrisorName(); System.out.print(" "); anna.printBokning();
+			}
+		} scanner.close();
+	}
+}			
